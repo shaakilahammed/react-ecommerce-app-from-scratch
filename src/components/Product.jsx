a0ae1d85a9467ba77ae23b424cf0313d5370218c
shaakilahammed <shaakilahammed@gmail.com>
2022-01-15 13:23:48 +0600

    ProductList Showing Page Added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -25,8 +25,8 @@ const Info = styled.div`
 const Container = styled.div`
   flex: 1;
   margin: 5px;
-  min-width: 280px;
-  height: 350px;
+  min-width: 380px;
+  height: 450px;
   display: flex;
   align-items: center;
   justify-content: center;
