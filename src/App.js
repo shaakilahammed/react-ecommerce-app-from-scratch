40e090ff1c5533204c5c8fa0f2da7b77e764ba9a
shaakilahammed <shaakilahammed@gmail.com>
2022-01-16 19:49:16 +0600

    Register Page Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from './pages/Home';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <div>
-      <Product />
+      <Register />
     </div>
   );
 };
---
+++ src/pages/Register.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.3),
+      rgba(255, 255, 255, 0.2)
+    ),
+    url('https://images.pexels.com/photos/5632402/pexels-photo-5632402.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=650&w=940')
+      center;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: #fff;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 10px;
+`;
+const Agreement = styled.span`
+  font-weight: 12px;
+  margin: 20px 0;
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="First Name" />
+          <Input placeholder="Last Name" />
+          <Input placeholder="User Name" />
+          <Input placeholder="Email" />
+          <Input placeholder="Password" />
+          <Input placeholder="Confirm Password" />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>Create</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
